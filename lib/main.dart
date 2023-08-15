3c79ef3d4c59695994529106697f559938bc2bf4
lowy-10pines <martin.loguancio@10pines.com>
2023-08-15 10:17:05 -0300

    Pre coso

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,86 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 /// Flutter code sample for [DecoratedBoxTransition].
 
 void main() => runApp(const DecoratedBoxTransitionExampleApp());
 
-class DecoratedBoxTransitionExampleApp extends StatelessWidget {
+class DecoratedBoxTransitionExampleApp extends StatefulWidget {
   const DecoratedBoxTransitionExampleApp({super.key});
 
   @override
+  State<DecoratedBoxTransitionExampleApp> createState() =>
+      _DecoratedBoxTransitionExampleAppState();
+}
+
+class _DecoratedBoxTransitionExampleAppState
+    extends State<DecoratedBoxTransitionExampleApp> {
+  bool value = true;
+  void onTap(bool? newValue) {
+    setState(() {
+      value = newValue ?? false;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      themeMode: ThemeMode.dark,
       home: ColoredBox(
-          color: Colors.white,
-          child: Center(child: DecoratedBoxTransitionExample())),
+        color: Colors.black,
+        child: Scaffold(
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FancySwitch(value: value, onChanged: onTap),
+                Switch.adaptive(value: value, onChanged: onTap),
+                Switch(value: value, onChanged: onTap),
+                Checkbox.adaptive(value: value, onChanged: onTap),
+                Checkbox(value: value, onChanged: onTap),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
-class DecoratedBoxTransitionExample extends StatefulWidget {
-  const DecoratedBoxTransitionExample({super.key});
+class FancySwitch extends StatefulWidget {
+  final void Function(bool?)? onChanged;
+  final bool value;
+  const FancySwitch({
+    super.key,
+    required this.value,
+    required this.onChanged,
+  });
 
   @override
-  State<DecoratedBoxTransitionExample> createState() =>
-      _DecoratedBoxTransitionExampleState();
+  State<FancySwitch> createState() => _FancySwitchState();
 }
 
-/// [AnimationController]s can be created with `vsync: this` because of
-/// [TickerProviderStateMixin].
-class _DecoratedBoxTransitionExampleState
-    extends State<DecoratedBoxTransitionExample> with TickerProviderStateMixin {
-  final DecorationTween _decorationsTween = DecorationTween(
-    begin: const ShapeDecoration(
-      color: Colors.white,
-      shape: CircleBorder(),
-      shadows: <BoxShadow>[
-        BoxShadow(
-          color: Colors.grey,
-          blurRadius: 10.0,
-          spreadRadius: 3.0,
-        ),
-      ],
-    ),
-    end: const ShapeDecoration(
-      color: Colors.yellow,
-      shape: CircleBorder(),
-      shadows: <BoxShadow>[
-        BoxShadow(
-          color: Colors.orange,
-          blurRadius: 10.0,
-          spreadRadius: 3.0,
-        ),
-      ],
-    ),
-  );
-
-  final AlignmentTween _alignmentTween = AlignmentTween(
-    begin: Alignment.centerLeft,
-    end: Alignment.centerRight,
-  );
-
-  final DecorationTween _backgroundAnimationTween = DecorationTween(
-    begin: BoxDecoration(
-      borderRadius: BorderRadius.circular(150),
-      color: Colors.black,
-    ),
-    end: BoxDecoration(
-      borderRadius: BorderRadius.circular(150),
-      color: Colors.lightBlue.shade100,
-    ),
-  );
-
+class _FancySwitchState extends State<FancySwitch>
+    with TickerProviderStateMixin {
   late final AnimationController _controller = AnimationController(
     vsync: this,
-    duration: const Duration(seconds: 3),
-  )..repeat(reverse: true);
+    duration: const Duration(milliseconds: 300),
+  );
 
   late final CurvedAnimation _curvedAnimation = CurvedAnimation(
     parent: _controller,
     curve: Curves.easeInOut,
   );
 
-  late final CurvedAnimation _expoCurvedAnimation = CurvedAnimation(
-    parent: _controller,
-    curve: Curves.easeOut,
-  );
+  @override
+  void initState() {
+    if (widget.value) {
+      _controller.forward(from: 1);
+    } else {
+      _controller.reverse(from: 0);
+    }
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -95,27 +90,335 @@ class _DecoratedBoxTransitionExampleState
 
   @override
   Widget build(BuildContext context) {
+    if (widget.value) {
+      _controller.forward();
+    } else {
+      _controller.reverse();
+    }
+    const double height = 40;
+    const double width = 70;
     return SizedBox(
-      height: 150,
-      width: 300,
-      child: DecoratedBoxTransition(
-        decoration: _backgroundAnimationTween.animate(_curvedAnimation),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-          child: AlignTransition(
-            alignment: _alignmentTween.animate(_curvedAnimation),
-            child: DecoratedBoxTransition(
-              decoration: _decorationsTween.animate(_curvedAnimation),
-              child: ClipOval(
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: Container(),
-                ),
+      height: height,
+      width: width,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(height / 2),
+        child: GestureDetector(
+          onTap: () => widget.onChanged?.call(!widget.value),
+          child: Animacion(
+            _curvedAnimation,
+            child: const FondoDeColor(
+              child: Stack(
+                children: [
+                  SolYLuna(containerSize: height),
+                  Estrellas(containerSize: height),
+                  Nube(containerSize: height),
+                ],
               ),
             ),
           ),
         ),
       ),
     );
+  }
+}
+
+class Animacion extends InheritedWidget {
+  final Animation<double> animacion;
+  const Animacion(
+    this.animacion, {
+    super.key,
+    required super.child,
+  });
+
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return oldWidget is Animacion && oldWidget.animacion != animacion;
+  }
+
+  static Animation<double> of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<Animacion>()!.animacion;
+  }
+}
+
+class FondoDeColor extends StatelessWidget {
+  final Widget child;
+  const FondoDeColor({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    final DecorationTween backgroundAnimationTween = DecorationTween(
+      begin: BoxDecoration(
+        borderRadius: BorderRadius.circular(150),
+        color: const Color.fromARGB(255, 5, 3, 87),
+      ),
+      end: BoxDecoration(
+        borderRadius: BorderRadius.circular(150),
+        color: Colors.lightBlue.shade100,
+      ),
+    );
+
+    return DecoratedBoxTransition(
+      decoration: backgroundAnimationTween.animate(Animacion.of(context)),
+      child: child,
+    );
+  }
+}
+
+class Nube extends StatelessWidget {
+  final double detailsSize;
+  const Nube({
+    super.key,
+    required double containerSize,
+  }) : detailsSize = containerSize / 6;
+
+  @override
+  Widget build(BuildContext context) {
+    final AlignmentTween alignmentTween = AlignmentTween(
+      begin: const Alignment(-3.8, 0.35),
+      end: const Alignment(-1.7, 0.35),
+    );
+
+    return AlignTransition(
+      alignment: alignmentTween.animate(Animacion.of(context)),
+      child: Padding(
+        padding: EdgeInsets.only(top: detailsSize, bottom: detailsSize),
+        child: AspectRatio(
+          aspectRatio: 1.2,
+          child: Center(
+            child: CustomPaint(
+              painter: CloudPainter(),
+              child: Container(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CloudPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final dx = size.width / 6;
+    final dy = size.height / 3;
+
+    final paint = Paint()
+      ..color = Colors.white
+      ..style = PaintingStyle.fill;
+
+    // Large circle
+    canvas.drawCircle(Offset(1.5 * dx, 1.5 * dy), 1.5 * dy, paint);
+
+    // Medium circle
+    canvas.drawCircle(Offset(5 * dx, 2 * dy), dy, paint);
+
+    // Small circle
+    canvas.drawCircle(Offset(7 * dx, 2.5 * dy), 0.5 * dy, paint);
+
+    canvas.drawRect(
+        Rect.fromPoints(Offset(1.5 * dx, 2.5 * dy), Offset(7 * dx, 3 * dy)),
+        paint);
+
+    canvas.drawRect(
+        Rect.fromPoints(Offset(1.5 * dx, 1.5 * dy), Offset(5 * dx, 3 * dy)),
+        paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+class Estrellas extends StatelessWidget {
+  final double size;
+  const Estrellas({
+    super.key,
+    required double containerSize,
+  }) : size = containerSize / 4;
+
+  final List<Alignment> _estrellas = const [
+    Alignment(-0.2, -0.6),
+    Alignment(-0.3, 0.6),
+    Alignment(0.5, -0.1),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: Tween<double>(begin: 1, end: -1).animate(Animacion.of(context)),
+      child: Align(
+        alignment: const Alignment(0.9, 0.5),
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: Stack(
+            children: _estrellas
+                .map(
+                  (alignment) => Align(
+                    alignment: alignment,
+                    child: RotationTransition(
+                      turns: Tween<double>(begin: -1, end: 0.5)
+                          .animate(Animacion.of(context)),
+                      child: DecoratedBox(
+                        decoration: const ShapeDecoration(
+                            shape: StarBorder(), color: Colors.white),
+                        child: SizedBox.square(
+                          dimension: size,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+                .toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SolYLuna extends StatelessWidget {
+  final double detailsSize;
+  const SolYLuna({
+    super.key,
+    required double containerSize,
+  }) : detailsSize = containerSize / 14;
+
+  @override
+  Widget build(BuildContext context) {
+    final DecorationTween decorationsTween = DecorationTween(
+      begin: const ShapeDecoration(
+        color: Colors.white,
+        shape: MoonBorder(),
+        shadows: <BoxShadow>[
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 10.0,
+            spreadRadius: 3.0,
+          ),
+        ],
+      ),
+      end: const ShapeDecoration(
+        color: Colors.yellow,
+        shape: RoundedRectangleBorder(),
+        shadows: <BoxShadow>[
+          BoxShadow(
+            color: Colors.orange,
+            blurRadius: 10.0,
+            spreadRadius: 3.0,
+          ),
+        ],
+      ),
+    );
+
+    final AlignmentTween alignmentTween = AlignmentTween(
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+    );
+
+    return Padding(
+      padding: EdgeInsets.all(detailsSize),
+      child: AlignTransition(
+        alignment: alignmentTween.animate(Animacion.of(context)),
+        child: DecoratedBoxTransition(
+          decoration: decorationsTween.animate(Animacion.of(context)),
+          child: ClipOval(
+            child: AspectRatio(
+              aspectRatio: 1,
+              child: Container(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MoonBorder extends ShapeBorder {
+  final BorderSide side;
+  const MoonBorder({this.side = BorderSide.none});
+
+  @override
+  EdgeInsetsGeometry get dimensions =>
+      EdgeInsets.all(math.max(side.strokeInset, 0));
+
+  @override
+  ShapeBorder scale(double t) => MoonBorder(side: side.scale(t));
+
+  @override
+  ShapeBorder? lerpFrom(ShapeBorder? a, double t) {
+    if (a is MoonBorder) {
+      return MoonBorder(
+        side: BorderSide.lerp(a.side, side, t),
+      );
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  @override
+  ShapeBorder? lerpTo(ShapeBorder? b, double t) {
+    if (b is MoonBorder) {
+      return MoonBorder(
+        side: BorderSide.lerp(side, b.side, t),
+      );
+    }
+    return super.lerpTo(b, t);
+  }
+
+  @override
+  Path getInnerPath(Rect rect, {TextDirection? textDirection}) {
+    return Path()..addOval(_adjustRect(rect).deflate(side.strokeInset));
+  }
+
+  @override
+  Path getOuterPath(Rect rect, {TextDirection? textDirection}) {
+    return Path()..addOval(_adjustRect(rect));
+  }
+
+  @override
+  void paintInterior(Canvas canvas, Rect rect, Paint paint,
+      {TextDirection? textDirection}) {
+    canvas.drawCircle(rect.center, rect.shortestSide / 2.0, paint);
+  }
+
+  @override
+  bool get preferPaintInterior => true;
+
+  MoonBorder copyWith({BorderSide? side}) {
+    return MoonBorder(
+      side: side ?? this.side,
+    );
+  }
+
+  @override
+  void paint(Canvas canvas, Rect rect, {TextDirection? textDirection}) {
+    switch (side.style) {
+      case BorderStyle.none:
+        break;
+      case BorderStyle.solid:
+        canvas.drawCircle(rect.center,
+            (rect.shortestSide + side.strokeOffset) / 2, side.toPaint());
+    }
+  }
+
+  Rect _adjustRect(Rect rect) {
+    return Rect.fromCircle(
+        center: rect.center, radius: rect.shortestSide / 2.0);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (other.runtimeType != runtimeType) {
+      return false;
+    }
+    return other is CircleBorder && other.side == side;
+  }
+
+  @override
+  int get hashCode => side.hashCode;
+
+  @override
+  String toString() {
+    return 'MoonBorder($side)';
   }
 }
